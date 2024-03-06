3fcb79342523a2f44862c8c69f088299b586da6d
SangeethCK <sangeethck7@gmail.com>
2024-03-06 09:23:55 +0530

    Splash ~ Image -> Ui Modified!

--- lib/presentation/screens/splash/screen_splash.dart
+++ lib/presentation/screens/splash/screen_splash.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:nayurveda_app/core/routes/routes.dart';
 import 'package:nayurveda_app/provider/sign-in/sign_in_provider.dart';
 
@@ -11,29 +11,41 @@ class ScreenSplash extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/logo_make_11_06_2023_115 1.png'),
+            image: const AssetImage(
+                'assets/images/logo_make_11_06_2023_115 1.png'),
             fit: BoxFit.fill,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(.65),
+              BlendMode.darken,
+            ),
           ),
         ),
-        child: Center(
-          child: FutureBuilder<void>(
-            future: Future.delayed(const Duration(seconds: 3)),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                final auth = ref.read(SignInProvider.signInProvider.notifier);
-                auth.loadToken().then((token) {
-                  if (token != null && token.isNotEmpty) {
-                    Navigator.pushReplacementNamed(context, home);
-                  } else {
-                    Navigator.pushReplacementNamed(context, login);
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/images/Asset 1 2.svg'),
+            Center(
+              child: FutureBuilder<void>(
+                future: Future.delayed(const Duration(seconds: 3)),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    final auth =
+                        ref.read(SignInProvider.signInProvider.notifier);
+                    auth.loadToken().then((token) {
+                      if (token != null && token.isNotEmpty) {
+                        Navigator.pushReplacementNamed(context, home);
+                      } else {
+                        Navigator.pushReplacementNamed(context, login);
+                      }
+                    });
                   }
-                });
-              }
-              return const CircularProgressIndicator();
-            },
-          ),
+                  return const SizedBox();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
